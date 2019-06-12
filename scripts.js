7980d12fc8e3eead96de5ac01ce09128db72dbc5
Harrison Cabral <44628007+Alchemyst19@users.noreply.github.com>
2019-06-12 19:38:55 -0400

    Add SourceNav

--- scripts.js
+++ scripts.js
@@ -1,11 +1,41 @@
 function hideRace (clickRace) {
     var x = document.getElementsByClassName("race");
-    var xtwo = x.length
-    console.log('x==>', x)
-    console.log('x[0]==>', x[0])
     for (let z=0; z<x.length; z++){
-        x[z].style.display = "none"
+        x[z].style.display = "none";
     }
     var y = document.getElementById(clickRace);
     y.style.display = "inline";
+}
+
+function showRace () {
+    var x = document.getElementsByClassName("race");
+    for (let z=0; z<x.length; z++){
+        x[z].style.display = "inline";
+    }
+}
+
+function hideSource (clickSource) {
+    var x = document.getElementsByClassName("race");
+    for (let z=0; z<x.length; z++){
+        x[z].style.display = "none";
+    }
+    var q = document.querySelectorAll('#racenav li');
+    for (let r=0; r<q.length; r++){
+        q[r].style.display = "none";
+    }
+    var y = document.getElementsByClassName(clickSource);
+    for (let v=0; v<y.length; v++){
+        y[v].style.display = "block";
+    }
+}
+
+function showSource () {
+    var x = document.getElementsByClassName("race");
+    for (let z=0; z<x.length; z++){
+        x[z].style.display = "inline";
+    }
+    var q = document.querySelectorAll('#racenav li');
+    for (let r=0; r<q.length; r++){
+        q[r].style.display = "block";
+    }
 }

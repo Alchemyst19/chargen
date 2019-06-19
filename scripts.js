748e89e070a3966e300ba87c398a46394f3dccb0
Harrison Cabral <44628007+Alchemyst19@users.noreply.github.com>
2019-06-19 19:54:46 -0400

    Add expandable nav box

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ function hideRace (clickRace) {
     }
     var y = document.getElementById(clickRace);
     y.style.display = "inline";
+    // toggleNav(2);
 }
 
 function showRace () {
@@ -12,6 +13,7 @@ function showRace () {
     for (let z=0; z<x.length; z++){
         x[z].style.display = "inline";
     }
+    // toggleNav(2);
 }
 
 function hideSource (clickSource) {
@@ -27,6 +29,7 @@ function hideSource (clickSource) {
     for (let v=0; v<y.length; v++){
         y[v].style.display = "block";
     }
+    // toggleNav(2);
 }
 
 function showSource () {
@@ -37,5 +40,24 @@ function showSource () {
     var q = document.querySelectorAll('#racenav li');
     for (let r=0; r<q.length; r++){
         q[r].style.display = "block";
+    }
+    // toggleNav(2);
+}
+
+function toggleNav (toggle) {
+    if(toggle == 1){
+        var x = document.getElementById('navbar');
+        x.style.display = "flex";
+        var y = document.getElementById('onbutton');
+        y.style.display = "none";
+        var z = document.getElementById('offbutton');
+        z.style.display = "flex";
+    }else{
+        var x = document.getElementById('navbar');
+        x.style.display = "none";
+        var y = document.getElementById('onbutton');
+        y.style.display = "flex";
+        var z = document.getElementById('offbutton');
+        z.style.display = "none";
     }
 }
